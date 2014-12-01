db436377c87dbe3c0d5161c348b82173633ea39c
Richard Albritton <richardalbritton@gmail.com>
2014-12-01 14:25:42 -0800

    Update Move.ino

--- HackEBot_Move/examples/Move/Move.ino
+++ HackEBot_Move/examples/Move/Move.ino
@@ -1,42 +1,62 @@
-#ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
-  #include <avr/power.h>
-#endif
-#include <HackEBot_Move.h>
+#include <Servo.h>
 
-// Set-up the movement controls using;
-// 		LeftPin = Left Servo Pin
-// 		RightPin = Right Servo Pin
-// HackEBot_Move robot(LeftPin, RightPin);
-   HackEBot_Move robot(12, 7);
+Servo RightS;
+Servo LeftS;
+
+int servoR = 13;
+int servoL = 8;
 
 void setup()
 {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
-  clock_prescale_set(clock_div_1);
-#endif
-  // ...
+  RightS.attach(servoR);
+  LeftS.attach(servoL);
 }
 
-int MoveSpeed = 200;
-int TurnSpeed = 150;
+int speed = map(50, 0, 100, 0, 90); // This remaps a value of 0%-100% to 0°-90°;
+int moveTime = 200;
+int turnTime = 50;
 
 void loop()
 {
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnR(16, TurnSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnR(16, TurnSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnR(16, TurnSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnR(16, TurnSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnL(16, MoveSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnL(16, TurnSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnL(16, TurnSpeed);
-  robot.MoveF(30, MoveSpeed);
-  robot.TurnL(16, TurnSpeed);
+  MoveF(speed, moveTime);
+  TurnR(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnR(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnR(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnR(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnL(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnL(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnL(speed, turnTime);
+  MoveF(speed, moveTime);
+  TurnL(speed, turnTime);
   delay(600);
+}
+
+void MoveF(int S, int T){ // Drive drive forward, S = driveSpeed, T = driveTime.
+  RightS.write(S);
+  LeftS.write(S + 90);
+  delay(T);
+}
+
+void MoveB(int S, int T){ // Drive drive forward, S = driveSpeed, T = driveTime.
+  RightS.write(S + 90);
+  LeftS.write(S);
+  delay(T);
+}
+
+void TurnL(int S, int T){ // Drive drive forward, S = driveSpeed, T = driveTime.
+  RightS.write(S);
+  LeftS.write(S);
+  delay(T);
+}
+
+void TurnR(int S, int T){ // Drive drive forward, S = driveSpeed, T = driveTime.
+  RightS.write(S + 90);
+  LeftS.write(S + 90);
+  delay(T);
 }
