833f8e7375cb7cd54bb415c6fd19e74bb1c514b9
Richard Albritton <richardalbritton@gmail.com>
2014-12-01 14:27:57 -0800

    Update HackEBot_Move.h

--- HackEBot_Move/HackEBot_Move.h
+++ HackEBot_Move/HackEBot_Move.h
@@ -2,33 +2,20 @@
 // 
 // Written by Richard Albritton for Hack-E-Bot Robotics, BSD license
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
 
 class HackEBot_Move {
   public:
-    HackEBot_Move(int L, int R);
-    long ServoSetup(int D, int F, int G, int H, int J, int K); //-- used to manually set the servo limits
-    void Calibrate(); //-- to Calibrate the servos
-    void MoveF(int S, int Z); //-- to drive forward, S = repeat number, Z = Speed.
-    void MoveB(int S, int Z); //-- to drive backward, S = repeat number, Z = Speed.
-    void TurnR(int S, int Z); //-- to turn right, S = repeat number, Z = Speed.
-    void TurnL(int S, int Z); //-- to turn left, S = repeat number, Z = Speed.
+    HackEBot_Move(int R, int L);
+    void Center(); //-- Dose servo.write(90) to test if the servo is centered.
+    void MoveF(int S, int T); //-- to drive forward, S = driveSpeed, T = driveTime.
+    void MoveB(int S, int T); //-- to drive backward, S = driveSpeed, T = driveTime.
+    void TurnR(int S, int T); //-- to turn right, S = driveSpeed, T = driveTime.
+    void TurnL(int S, int T); //-- to turn left, S = driveSpeed, T = driveTime.
 
   private:
-   int servoL;
    int servoR;
-   int steps;
-   int speed;
-   int UpperL;
-   int CenterL;
-   int LowerL;
-   int UpperR;
-   int CenterR;
-   int LowerR;
-   int LShift;
-   int RShift;
+   int servoL;
+   int driveSpeed;
+   int driveTime;
+
 };
